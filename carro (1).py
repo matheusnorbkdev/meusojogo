bfc804f69c86fc4824911b15c7f50ac460ab22dd
norbak matheus <matheus.nc10@aluno.ifsc.edu.br>
2024-10-09 10:54:33 -0300

    Update carro (1).py

--- carro (1).py
+++ carro (1).py
@@ -3,6 +3,7 @@ import os
 import random
 clear = 'cls' if os.name == 'nt' else 'clear'
 x = " "
+y = " "
 N = input("qual seu apelido jogador 1 digite 5 letras ou menos ")
 N2 = input("qual seu apelido jogador 2 digite 5 letras ou menos ")
 for c in range(20):
@@ -11,14 +12,13 @@ for c in range(20):
     print(x + "▆   ",N,"   ▆ ")
     print(x + "▆             ▆ ")
     print(x + "▆             ▆" )
-    print(x + "▆▆▆▆◯▆▆▆▆▆◯▆▆▆▆")
-    x = x + random.choice([ " ", "  ", "   "])
+    print(x + "^^^^◯^^^^^^◯^^^^")
+    x = x + random.choice([" ", "  ", "   "])
     time.sleep(0.1)
-    print(x + "▆▆▆▆▆▆▆▆▆▆▆▆▆")
-    print(x + "▆   ",N2,"   ▆ ")
-    print(x + "▆             ▆ ")
-    print(x + "▆             ▆" )
-    print(x + "▆▆▆▆◯▆▆▆▆▆◯▆▆▆▆")
-    x = x + random.choice([ " ", "  ", "   "])
+    print(y + "▆▆▆▆▆▆▆▆▆▆▆▆▆")
+    print(y + "▆   ",N2,"   ▆ ")
+    print(y + "▆             ▆ ")
+    print(y + "▆             ▆" )
+    print(y + "^^^^◯^^^^^^◯^^^^")
+    y = y  + random.choice([ " ", "  ", "   "])
     time.sleep(0.1)
-    
